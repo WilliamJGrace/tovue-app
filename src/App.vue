1f52ca6d606f2b76b147fe9d351f9c186bcaf409
WilliamJGrace <williamjgrace@outlook.com>
2020-10-26 15:40:03 +0000

    manually added todo's

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,28 @@
 
 export default {
   name: 'App',
+  data() {
+    return {
+      todos: [
+        {id: 1,
+        title: "Go to the Gym",
+        completed: false
+        },
+        {id: 2,
+        title: "Walk Dog",
+        completed: false
+        },
+        {id: 3,
+        title: "Go shopping",
+        completed: false
+        },
+        {id: 4,
+        title: "Call the vet",
+        completed: false
+        }
+      ]
+    }
+  }
 
 }
 </script>
