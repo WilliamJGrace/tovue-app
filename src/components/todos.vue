9725c7523355f50f40f73601c7645083a20a5725
WilliamJGrace <williamjgrace@outlook.com>
2020-10-26 16:35:08 +0000

    adding todos as props

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
         <h2>My todolist</h2>
+        <Todo></Todo>
     </div>
 </template>
 
 <script>
     import Todo from './todo'
-    
+
     export default {
-        name: 'todos',
+        name: 'Todos',
         components: {
             Todo
-        }
+        },
+        props: [
+            "todos"
+        ]
         
     }
 </script>
