a96e92ec9ce026b9de2cd8304f8b2972898bfaac
WilliamJGrace <williamjgrace@outlook.com>
2020-10-26 16:00:42 +0000

    todos.vue

---
+++ src/components/todos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>My todolist</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'todos',
+        components: {
+            Todo
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
