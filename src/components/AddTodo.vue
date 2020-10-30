df690b3d4e5e1d1481e510859c8cd3851e210cc0
WilliamJGrace <williamjgrace@outlook.com>
2020-10-30 09:25:43 +0000

    added form to template

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title">
+            <button type="submit">Add</button>
+        </form>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
